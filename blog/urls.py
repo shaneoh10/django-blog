75260dd0474805fb8c2b5a1d0207d94adc8a3ed8
shaneoh10 <shaneaghada@gmail.com>
2021-07-23 09:39:20 +0000

    Add delete functionality and user authentication

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import HomeView, ArticleDetailView, AddPostView, EditPostView
+from .views import (
+    HomeView, ArticleDetailView, AddPostView, EditPostView, DeletePostView)
 
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
     path('article/<int:pk>', ArticleDetailView.as_view(), name="article"),
     path('add_post/', AddPostView.as_view(), name="add_post"),
     path('article/edit/<int:pk>', EditPostView.as_view(), name="edit_post"),
+    path('article/<int:pk>/delete', DeletePostView.as_view(), name="delete_post"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import (
+    ListView, DetailView, CreateView, UpdateView, DeleteView)
 from .models import Post
 from .forms import PostForm, EditPostForm
+from django.urls import reverse_lazy
 
 
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
-
+    ordering = ['-post_date']
 
 class ArticleDetailView(DetailView):
     model = Post
@@ -24,3 +26,9 @@ class EditPostView(UpdateView):
     model = Post
     form_class = EditPostForm
     template_name = 'edit_post.html'
+
+
+class DeletePostView(DeleteView):
+    model = Post
+    template_name = 'delete_post.html'
+    success_url = reverse_lazy('home')
